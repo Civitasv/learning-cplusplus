8cbe9f27414bc6822b11b760b5d73eb88185471a
Civitasv <hscivitasv@gmail.com>
2022-08-17 10:27:55 +0800

    [episode 3] event loop

---
+++ game dev/really-easy-game/src/render_window.cc
@@ -0,0 +1,21 @@
+﻿#include "include/render_window.h"
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+#include <iostream>
+
+RenderWindow::RenderWindow(const char *p_title, int p_w, int p_h)
+    : window(nullptr), renderer(nullptr) {
+  window =
+      SDL_CreateWindow(p_title, SDL_WINDOWPOS_UNDEFINED,
+                       SDL_WINDOWPOS_UNDEFINED, p_w, p_h, SDL_WINDOW_SHOWN);
+  if (window == nullptr) {
+    std::cout << "Window failed to init. Error: " << SDL_GetError()
+              << std::endl;
+  }
+
+  renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+}
+
+void RenderWindow::CleanUp() { SDL_DestroyWindow(window); }
--- game dev/really-easy-game/src/run.cc
+++ game dev/really-easy-game/src/run.cc
@@ -1,6 +1,32 @@
-﻿#include <iostream>
+﻿#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 
-int main() {
-  std::cout << "hello" << '\n';
+#include <iostream>
+
+#include "include/render_window.h"
+
+int main(int argc, char *argv[]) {
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    std::cout << "HEY.. SDL_Init FAILED: " << SDL_GetError() << std::endl;
+  }
+  if (!(IMG_Init(IMG_INIT_PNG))) {
+    std::cout << "HEY.. IMG_Init FAILED: " << SDL_GetError() << std::endl;
+  }
+
+  RenderWindow window("Game v1.0", 1280, 720);
+  bool game_running = true;
+  SDL_Event event;
+
+  // event loop
+  while (game_running) {
+    while (SDL_PollEvent(&event)) {
+      if (event.type == SDL_QUIT) {
+        game_running = false;
+      }
+    }
+  }
+
+  window.CleanUp();
+  SDL_Quit();
   return 0;
 }
