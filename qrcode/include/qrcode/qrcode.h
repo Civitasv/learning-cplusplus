a062ee633ed2d67573874b3e25075059aced2d79
Civitasv <hscivitasv@gmail.com>
2022-12-01 10:03:07 +0800

    commit for now

--- qrcode/include/qrcode/qrcode.h
+++ qrcode/include/qrcode/qrcode.h
@@ -10,6 +10,17 @@ std::vector<char> alphanumeric_values = {
     'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
     'U', 'V', 'W', 'X', 'Y', 'Z', ' ', '$', '%', '*', '+', '-', '.', '/', ':'};
 
+// version, mode, error correction level
+int capacities[40][4][4] = {
+    {{41, 34, 27, 17}, {25, 20, 16, 10}, {17, 14, 11, 7}, {10, 8, 7, 4}},
+    {{77, 63, 48, 34}, {47, 38, 29, 20}, {32, 26, 20, 14}, {20, 16, 12, 8}},
+    {{127, 101, 77, 58}, {77, 61, 47, 35}, {53, 42, 32, 24}, {32, 26, 20, 15}},
+    {{187, 149, 111, 82}, {114, 90, 67, 50}, {78, 62, 46, 34}, {48,38, 28, 21}},
+    {{255, 202, 144, 106}, {154, 122, 87, 64}, {106, 84, 60, 44}, {65, 52,37,27}},
+    {{322, 255, 178, 139}, {195, 154, 108, 84}, {134, 106, 74, 58},{82, 65, 45, 36}},
+    {{}}
+};
+
 struct ImageInfo {
   unsigned char r;  ///< r
   unsigned char g;  ///< g
@@ -23,7 +34,7 @@ class QRCode {
  public:
   enum EncodingMode {
     // for decimal digits 0 through 9
-    NUMERIC,
+    NUMERIC = 0,
     // for decimal digits 0 through 9, as well as uppercase
     // letters, and $, %, *, +, ., / and : as well as a space
     ALPHANUMERIC,
@@ -36,10 +47,10 @@ class QRCode {
   };
 
   enum ECC {
-    L,  // recovers 7% of data
-    M,  // recovers 15% of data
-    Q,  // recovers 25% of data
-    H   // recovers 30% of data
+    L = 0,  // recovers 7% of data
+    M,      // recovers 15% of data
+    Q,      // recovers 25% of data
+    H       // recovers 30% of data
   };
 
  public:
@@ -89,7 +100,6 @@ class QRCode {
     // 1. choose the error correction level
     ECC ecc = L;
     // 2. determine the smallest version for the data
-    
   }
 };
 }  // namespace qrcode
